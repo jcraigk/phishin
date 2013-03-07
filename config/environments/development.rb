6d91a834caa48bb882d5144b61573c2a748c6b18
jcraigk <jcraigk@gmail.com>
2013-03-06 20:24:22 -0800

    config fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,6 @@ Phishin::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.default_url_options = { :host => 'phish.in' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Phishin::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'phish.in' }
   
 end
