641673382397a622791be9a7f82cdc020da7eb94
jcraigk <jcraigk@gmail.com>
2013-03-08 21:22:55 -0800

    aesthetic updates"

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,9 +1,12 @@
 class PlaylistsController < ApplicationController
 
   def playlist
+    @num_tracks = 0
+    @duration = 0
     if session['playlist']
       tracks_by_id = Track.find(session['playlist']).index_by(&:id) # Gives you a hash indexed by ID
       @tracks = session['playlist'].collect {|id| tracks_by_id[Integer(id,10)] }
+      @num_tracks = @tracks.size
       @duration = @tracks.map(&:duration).inject(0, &:+)
     end
     render layout: false if request.xhr?
