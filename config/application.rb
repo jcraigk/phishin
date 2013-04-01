c70a4f643acc9a4eebc85b41e93d538abf81c73f
jcraigk <jcraigk@gmail.com>
2013-03-31 17:25:57 -0700

    another devise test

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,16 @@ module Phishin
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    ActionMailer::Base.smtp_settings = {
+      :address                => 'smtp.gmail.com',
+      :port                   => 587,
+      :domain                 => 'gmail.com',
+      :user_name              => 'phish.in.music@gmail.com',
+      :password               => 'omeg544s',
+      :authentication         => 'plain',
+      :enable_starttls_auto   => true
+    }
       
   end
 end
