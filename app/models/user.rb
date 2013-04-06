85e8aeabe75ec43b316fe772837243d23d325d2f
jcraigk <jcraigk@gmail.com>
2013-04-05 18:06:01 -0700

    username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, format: { with: /[A-Za-z0-9_]/, message: ': only letters, numbers, and underscores are allowed' }
   
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
