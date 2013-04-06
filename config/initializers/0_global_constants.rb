78d4d456f0a7b51388e9ea5e1376ac76ef42e1f4
jcraigk <jcraigk@gmail.com>
2013-04-05 17:42:50 -0700

    album cache size

--- config/initializers/0_global_constants.rb
+++ config/initializers/0_global_constants.rb
@@ -11,7 +11,7 @@ APP_NAME              = "phish.in'"                             # App name appea
 APP_EMAIL             = "phish.in.music@gmail.com"              # Main contact email
 DEVISE_EMAIL_FROM     = "phish.in' <noreply@phish.in>"          # From address for Devise emails
 
-ALBUM_CACHE_MAX_SIZE  = 10.gigabytes                            # Maximum size of album attachment cache
+ALBUM_CACHE_MAX_SIZE  = 50.gigabytes                            # Maximum size of album attachment cache
 ALBUM_TIMEOUT         = 10.seconds                              # Time to wait for album creation before telling user
 
 if Rails.env == 'development' || Rails.env == 'test'
