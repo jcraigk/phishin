2bd07812d40dbc1de2c955beaca4348ca66559f8
jcraigk <jcraigk@gmail.com>
2014-07-27 20:34:56 -0700

    Disable devise confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :likes, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   
   validates :username, uniqueness: true, format: { with: /^[A-Za-z0-9_]{4,15}$/, message: 'may contain only letters, numbers, and underscores; must be unique; and must be 4 to 15 characters long' }
   
