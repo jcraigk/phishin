bc73c9afcab843da3010d454e785bec552bf9a31
jcraigk <jcraigk@gmail.com>
2015-05-09 12:59:50 -0700

    Add range to CORS headers to support gapless playback

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -56,7 +56,7 @@ module Api
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, range'
         headers['Access-Control-Max-Age'] = '1728000'
       end
     end
