8e3e10e1741fa446d8e76c1829a4f729118d6b7d
jcraigk <jcraigk@gmail.com>
2015-09-07 15:23:32 -0600

    Remove debug

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -201,7 +201,6 @@ class PlaylistsController < ApplicationController
           if session[:loop]
             render json: { success: true, track_id: playlist.first }
           else
-            raise playlist.inspect
             render json: { success: false, msg: 'End of playlist' }
           end
         else
