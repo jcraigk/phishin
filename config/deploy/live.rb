86087354b9cdf3de019c897e6a4b1909c7c9b8f3
jcraigk <jcraigk@gmail.com>
2015-11-11 23:44:46 -0800

    Deploy to new server

--- config/deploy/live.rb
+++ config/deploy/live.rb
@@ -1,6 +1,7 @@
-server 'www.phish.in', user: 'jcraigk', roles: %w{web app db}
+server '66.33.207.45', user: 'jcraigk', roles: %w{web app db}
 
 set :rails_env,     :production
-set :deploy_to,     '/var/www/html.phish.in'
+set :deploy_to,     '/var/www/apps/phishin'
 set :audio_path,    '/var/www/app_content/phishin/tracks/audio_files'
 set :linked_files,  %w(config/database.yml)
+set :tmp_dir,       '/home/jcraigk/tmp'
