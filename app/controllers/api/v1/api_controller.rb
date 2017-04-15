845d78b780abd9d54e4e0677a807f5df2dbb1dce
Justin Craig-Kuhn <jcraigk@gmail.com>
2017-04-14 23:24:43 -0700

    Capitalize Range in CORS headers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -71,7 +71,7 @@ module Api
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, range'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Range'
         headers['Access-Control-Max-Age'] = '1728000'
       end
 
