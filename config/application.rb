a4bdd3a49a718e42e6c216893b1ab5eee4335af4
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-06-10 18:04:10 -0700

    Use permit_all_parameters for now

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,8 @@ module Phishin
       soundmanager2.swf
       soundmanager2_flash9.swf
     ]
+    # TODO: Remove this!  Was causing errors on
+    # http://phish.in/search-map?lat=44.4758825&lng=-73.21207199999998&distance=10 for example
+    config.action_controller.permit_all_parameters = true
   end
 end
