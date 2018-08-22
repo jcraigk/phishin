0fb97c0308a81015a425f4c21b9bc81c3a9c0235
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-08-22 09:32:40 -0700

    Compile missing assets in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
