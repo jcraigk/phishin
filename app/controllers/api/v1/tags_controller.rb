f4772603862fb57aa488d2d17a7ad14c671479d0
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-10-28 16:24:25 -0500

    Fix tag json output

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -4,11 +4,11 @@ class Api::V1::TagsController < Api::V1::ApiController
   caches_action :show, expires_in: CACHE_TTL
 
   def index
-    respond_with_success get_data_for(Tag)
+    respond_with_success(get_data_for(Tag), serialize_method: :as_json)
   end
 
   def show
     tag = Tag.where(name: params[:id]).or(Tag.where(id: params[:id]))
-    respond_with_success(tag, serialize_method: :as_json_api)
+    respond_with_success tag
   end
 end
