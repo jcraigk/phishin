31e73c7918dde69f068c11fb091b6fbcd1a466df
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-10-28 15:09:41 -0500

    Remove tracks from songs index api endpoint

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -4,14 +4,14 @@ class Api::V1::SongsController < Api::V1::ApiController
   caches_action :show, cache_path: proc { |c| c.params }, expires_in: CACHE_TTL
 
   def index
-    respond_with_success get_data_for(Song.relevant)
+    respond_with_success(get_data_for(Song.relevant), serialize_method: :as_json)
   end
 
   def show
-    show = Song.where(id: params[:id])
+    song = Song.where(id: params[:id])
                .or(Song.where(slug: params[:id]))
                .includes(tracks: :show)
                .first
-    respond_with_success show
+    respond_with_success song
   end
 end
