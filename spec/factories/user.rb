5d72f04a83fabee771e422781e719e42d8834d8f
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-11-17 12:07:54 -0800

    Fix factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :user do
     sequence(:username) { |n| "username#{n}" }
     email { Faker::Internet.email }
-    password 'password'
-    password_confirmation 'password'
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
