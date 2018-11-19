16ab9cc9150955ee38fab43f7b90ac62bcbdd65c
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2018-11-18 18:15:00 -0800

    Remove application layer CORS headers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 class Api::V1::ApiController < ActionController::Base
   before_action :attempt_user_authorization!
-  before_action :cors_preflight_check
-  after_action :cors_set_access_control_headers
   after_action :set_json_content_type
 
   protected
@@ -72,23 +70,6 @@ class Api::V1::ApiController < ActionController::Base
   end
 
   private
-
-  def cors_preflight_check
-    return unless request.method == :options
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = '1728000'
-    render text: '', content_type: 'text/plain'
-  end
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Range'
-    headers['Access-Control-Max-Age'] = '1728000'
-  end
 
   def attempt_user_authorization!
     return unless params[:user].present? &&
