cd709d68f6c3939ba86a2f53ab256eda1833b74e
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2018-11-22 02:27:26 -0800

    Remove Devise spec support

--- spec/support/devise.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
-  config.include Devise::Test::IntegrationHelpers, type: :feature
-end
