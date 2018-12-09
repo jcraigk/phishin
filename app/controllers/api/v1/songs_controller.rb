1ec811e56f58f1361695f12e205de0ceb42b8972
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-12-08 19:11:33 -0800

    Optimize API queries using bullet gem (#26)

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -4,10 +4,16 @@ class Api::V1::SongsController < Api::V1::ApiController
   caches_action :show, cache_path: proc { |c| c.params }, expires_in: CACHE_TTL
 
   def index
-    respond_with_success(get_data_for(Song.relevant), serialize_method: :as_json)
+    respond_with_success get_data_for(Song.relevant), serialize_method: :as_json
   end
 
   def show
-    respond_with_success Song.includes(tracks: :show).friendly.find(params[:id])
+    respond_with_success song_scope.friendly.find(params[:id])
+  end
+
+  private
+
+  def song_scope
+    Song.includes(tracks: %i[show songs tags])
   end
 end
