c96a56a4ac2e18e73e8f1c3dee9cef4844e63075
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2018-12-16 22:55:09 -0800

    Remove venues.past_names

---
+++ db/migrate/20181217065408_remove_venue_past_names.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class RemoveVenuePastNames < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :venues, :past_names, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_080858) do
+ActiveRecord::Schema.define(version: 2018_12_17_065408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,7 +205,6 @@ ActiveRecord::Schema.define(version: 2018_12_14_080858) do
 
   create_table "venues", id: :serial, force: :cascade do |t|
     t.string "name", limit: 255
-    t.string "past_names", limit: 255
     t.string "city", limit: 255
     t.string "state", limit: 255
     t.string "country", limit: 255
