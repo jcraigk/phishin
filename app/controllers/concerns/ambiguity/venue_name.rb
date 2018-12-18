ee4130190f2b38c838a3d853c1d6d164aad1198a
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2018-12-17 19:21:02 -0800

    Tweaks

--- app/controllers/concerns/ambiguity/venue_name.rb
+++ app/controllers/concerns/ambiguity/venue_name.rb
@@ -26,16 +26,12 @@ module Ambiguity::VenueName
   end
 
   def prev_venue
-    Venue.where('name < ?', venue.name)
-         .order(name: :desc)
-         .first ||
+    Venue.where('name < ?', venue.name).order(name: :desc).first ||
       Venue.order(name: :desc).first
   end
 
   def next_venue
-    Venue.where('name > ?', venue.name)
-         .order(name: :asc)
-         .first ||
+    Venue.where('name > ?', venue.name).order(name: :asc).first ||
       Venue.order(name: :asc).first
   end
 end
