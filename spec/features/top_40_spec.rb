3a50c13db94091c11cf097498d44866a6616d765
Justin Craig-Kuhn <jcraigk@gmail.com>
2018-12-17 20:59:50 -0800

    Show all songs in track context dropdown (#53)

--- spec/features/top_40_spec.rb
+++ spec/features/top_40_spec.rb
@@ -2,9 +2,11 @@
 require 'rails_helper'
 
 describe 'Top 40', :js do
+  let(:tour) { create(:tour) }
+  let(:shows) { create_list(:show, 42, :with_likes, tour: tour) }
+
   before do
-    create_list(:show, 42, :with_likes)
-    create_list(:track, 42, :with_likes)
+    create_list(:track, 42, :with_likes, show: shows.first)
   end
 
   it 'visit Top 40 page' do
