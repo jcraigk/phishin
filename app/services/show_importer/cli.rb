4c6514aed73342c0dc24d3f5ab312b168c3185a3
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2019-06-23 16:17:38 -0700

    Add exit command to shows:import task

--- app/services/show_importer/cli.rb
+++ app/services/show_importer/cli.rb
@@ -16,7 +16,7 @@ class ShowImporter::Cli
   def main_menu
     puts "\n#{orch.show.date} - #{orch.show.venue.name} - #{orch.show.venue.location}\n\n"
     orch.pp_list
-    puts "\n\nTrack #, (f)ilenames, (l)ist, (i)nsert, (d)elete, (s)ave: "
+    puts "\n\nTrack #, (f)ilenames, (l)ist, (i)nsert, (d)elete, (s)ave, e(x)it: "
   end
 
   def print_filenames
@@ -158,6 +158,8 @@ class ShowImporter::Cli
       delete_track
     when 's'
       orch.save
+    when 'x'
+      exit
     end
   end
 end
