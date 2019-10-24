5c2b394d7d0b13019db0d4564d51a5a0cf336a86
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2019-10-24 10:36:52 -0700

    Load Rails 6.0 app defaults

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ Bundler.require(*Rails.groups)
 
 module Phishin
   class Application < Rails::Application
+    config.load_defaults 6.0
     config.action_controller.permit_all_parameters = true
   end
 end
