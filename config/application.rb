8118df55d7087324d1dc706629163257d47154be
Justin Craig-Kuhn <jcraigk@gmail.com>
2021-04-11 16:03:03 -0700

    Add active_storage create migration (#220)

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'active_record/railtie'
+require 'active_storage/engine'
 require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)
