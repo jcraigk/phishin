c5ff4cb3afec18df288ca7f16d54fda4e3c2712e
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2021-04-12 19:49:53 -0700

    Drop ActiveStorage tables

---
+++ db/migrate/20210413024707_remove_activestorage_tables.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+class RemoveActivestorageTables < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :active_storage_variant_records
+    drop_table :active_storage_attachments
+    drop_table :active_storage_blobs
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_223309) do
+ActiveRecord::Schema.define(version: 2021_04_13_024707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
 
   create_table "api_keys", force: :cascade do |t|
     t.string "name", null: false
@@ -227,6 +199,7 @@ ActiveRecord::Schema.define(version: 2021_04_11_223309) do
     t.string "slug", limit: 255, null: false
     t.integer "tags_count", default: 0
     t.integer "jam_starts_at_second"
+    t.datetime "storage_converted_at"
     t.index ["jam_starts_at_second"], name: "index_tracks_on_jam_starts_at_second"
     t.index ["likes_count"], name: "index_tracks_on_likes_count"
     t.index ["show_id", "position"], name: "index_tracks_on_show_id_and_position", unique: true
@@ -283,6 +256,4 @@ ActiveRecord::Schema.define(version: 2021_04_11_223309) do
     t.index ["slug"], name: "index_venues_on_slug", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
