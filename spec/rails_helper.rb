2fb32b70caccbaec911a4b880abcab6fe44474df
Justin Craig-Kuhn <jcraigk@gmail.com>
2022-05-20 23:35:10 -0700

    Bundle update (Rails 7.0.3) (#279)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,7 @@ end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.order = 'random'
