733f27d9367c39dfd98d40901c7186072721d108
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2023-10-05 19:10:18 -0700

    react_on_rails install

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,10 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
+  # Ensure that if we are running js tests, we are using latest webpack assets
+  # This will use the defaults of :js and :server_rendering meta tags
+  ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)
+
   config.fixture_path = "#{Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
