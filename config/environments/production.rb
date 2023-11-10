bfc0c6645dca6037ef58b5f2196d44149c59c0e5
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2023-11-10 15:04:57 -0800

    Bundle update (Rails 7.1.2)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-
   # Code is not reloaded between requests
   config.cache_classes = true
 
