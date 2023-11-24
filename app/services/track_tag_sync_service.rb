ad9b10dc3edc914c1c1af30267bc3de4668a29cf
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2023-11-23 16:58:05 -0800

    Improve tagin:sync task

--- app/services/track_tag_sync_service.rb
+++ app/services/track_tag_sync_service.rb
@@ -4,34 +4,37 @@ class TrackTagSyncService
   include ActionView::Helpers::SanitizeHelper
   include Syncable
 
-  attr_reader :tag, :data, :track, :created_ids, :updated_ids, :dupes
+  attr_reader :tag, :data, :track, :created_ids, :updated_ids, :dupes, :missing_tracks
 
   def initialize(tag_name, data)
     @data = data
     @tag = Tag.find_by!(name: tag_name)
-    @dupes = []
+    @missing_tracks = []
     @created_ids = []
     @updated_ids = []
   end
 
   def call
     sync_track_tags
-
-    if dupes.any?
-      puts
-      puts 'DUPES:'
-      puts dupes
-    end
-
-    puts
-    puts "#{created_ids.size} tags created"
-    puts "#{updated_ids.size} tags updated"
+    print_summary
   end
 
   private
 
-  def destroy_existing_track_tags
-    TrackTag.where(tag:).destroy_all
+  def print_errors
+    return if missing_tracks.none?
+
+    puts
+    puts 'MISSING TRACKS:'
+    puts missing_tracks
+  end
+
+  def print_summary
+    print_errors
+
+    puts
+    puts "#{created_ids.size} tags created"
+    puts "#{updated_ids.size} tags updated"
   end
 
   def sync_track_tags
@@ -58,16 +61,24 @@ class TrackTagSyncService
   end
 
   def create_track_tag(row)
-    print '.'
-    @created_ids <<
-      TrackTag.create!(
-        tag:,
-        track:,
-        starts_at_second: seconds_or_nil(row['Starts At']),
-        ends_at_second: seconds_or_nil(row['Ends At']),
-        notes: sanitize_str(row['Notes']),
-        transcript: sanitize_str(row['Transcript'])
-      ).id
+    if track.blank?
+      @missing_tracks << row['URL']
+      print 'x'
+    else
+      print '.'
+      @created_ids << new_track_tag_id(row)
+    end
+  end
+
+  def new_track_tag_id(row)
+    TrackTag.create!(
+      tag:,
+      track:,
+      starts_at_second: seconds_or_nil(row['Starts At']),
+      ends_at_second: seconds_or_nil(row['Ends At']),
+      notes: sanitize_str(row['Notes']),
+      transcript: sanitize_str(row['Transcript'])
+    ).id
   end
 
   def update_track_tag(track_tag, row)
