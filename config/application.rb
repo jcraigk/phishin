a6b329b5d4807b3e068aa79a01c9082da4345555
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2024-08-09 19:49:38 -0700

    Use default key_generator_hash_digest_class

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Phishin
     config.load_defaults 7.2
 
     # For backwards compatibility with earlier Devise logins
-    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
+    # config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
     config.action_dispatch.cookies_serializer = :marshal # :json_allow_marshal
 
     config.hosts << ENV.fetch("WEB_HOST", nil) if ENV["WEB_HOST"].present?
