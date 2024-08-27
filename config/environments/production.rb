37fd54918537fca04349da8d3f2b734081e336f6
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2024-08-26 18:24:23 -0700

    eager_load false in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
   config.cache_classes = true
   config.consider_all_requests_local = false
-  config.eager_load = true
+  config.eager_load = false
   config.force_ssl = true
   config.i18n.fallbacks = true
   config.require_master_key = true
