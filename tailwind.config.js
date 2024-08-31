6f8284ca4da98c313ea2460b66b517e4362aa917
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2024-08-30 20:24:27 -0700

    Try to add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.jsx',
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
