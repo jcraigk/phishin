6fc180c3baa16ff6a63b753c65c2d29b2d69f53a
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2024-08-31 09:39:42 -0700

    Add tailwindcss

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,7 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 import '../stylesheets/react.css.scss';
 
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
+import HelloWorld from '../components/HelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
