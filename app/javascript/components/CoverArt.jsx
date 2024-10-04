9eec26e0afbec4cb4736d5d6ec9ae3b6c1073fac
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2024-10-03 23:17:06 -0700

    Fixes

--- app/javascript/components/CoverArt.jsx
+++ app/javascript/components/CoverArt.jsx
@@ -4,21 +4,30 @@ const CoverArt = ({ coverArtUrls, albumCoverUrl, openAppModal, size }) => {
   const handleOpenModal = () => {
     if (!openAppModal) return;
     const modalContent = (
-      <div className="large-album-art">
-        <img src={albumCoverUrl || coverArtUrls?.medium} alt="Cover art" />
-      </div>
+      <>
+        {albumCoverUrl && (
+          <div className="large-album-art">
+            <img src={albumCoverUrl} alt="Album cover" />
+          </div>
+        )}
+        {coverArtUrls?.medium && (
+          <div className="large-album-art mt-3">
+            <img src={coverArtUrls?.medium} alt="Cover art" />
+          </div>
+        )}
+      </>
     );
     openAppModal(modalContent);
   };
 
   return (
-    <span className="cover-art cover-art-modal-trigger" onClick={handleOpenModal}>
+    <div className="cover-art cover-art-modal-trigger" onClick={handleOpenModal}>
       <img
         src={size === "large" ? coverArtUrls?.medium : coverArtUrls?.small}
         alt="Cover art"
         className={size === "large" ? "cover-art-large" : "cover-art-small"}
       />
-    </span>
+    </div>
   );
 };
 
