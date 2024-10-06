076e2c4f9aed9604c9985bca99422e8d02ee8ee0
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2024-10-06 13:33:39 -0700

    Fix

--- app/jobs/album_zip_cleanup_job.rb
+++ app/jobs/album_zip_cleanup_job.rb
@@ -6,14 +6,15 @@ class AlbumZipCleanupJob
 
     # Continue deleting the oldest blobs until we are under the disk limit
     while total_size > App.album_zip_disk_limit
-      oldest_blob = ActiveStorage::Blob
-        .joins(:attachments)
-        .where(active_storage_attachments: { name: "album_zip", record_type: "Show" })
-        .order(:created_at)
-        .first
+      oldest_blob =
+        ActiveStorage::Blob.joins(:attachments)
+          .where(active_storage_attachments: { name: "album_zip", record_type: "Show" })
+          .order(:created_at)
+          .first
       break unless oldest_blob
+      byte_size = oldest_blob.byte_size
       oldest_blob.purge
-      total_size -= oldest_blob.byte_size
+      total_size -= byte_size
     end
   end
 
