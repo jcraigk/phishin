f59bde32a3caf34f5f1f42ac84b864e8b45276b6
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2024-10-07 14:44:04 -0700

    Fix album zip job

--- app/jobs/album_zip_cleanup_job.rb
+++ app/jobs/album_zip_cleanup_job.rb
@@ -5,7 +5,7 @@ class AlbumZipCleanupJob
     total_size = get_total_size
     return if total_size <= App.album_zip_disk_limit
 
-    # Continue deleting the oldest blobs until we are under the disk limit
+    # Continue deleting the oldest attachments until we are under the disk limit
     while total_size > App.album_zip_disk_limit
       oldest_blob =
         ActiveStorage::Blob.joins(:attachments)
@@ -13,8 +13,10 @@ class AlbumZipCleanupJob
           .order(:created_at)
           .first
       break unless oldest_blob
+      attachment = oldest_blob.attachments.find_by(name: "album_zip", record_type: "Show")
+      break unless attachment
       byte_size = oldest_blob.byte_size
-      oldest_blob.purge
+      attachment.destroy
       total_size -= byte_size
     end
   end
--- app/jobs/album_zip_job.rb
+++ app/jobs/album_zip_job.rb
@@ -6,9 +6,10 @@ class AlbumZipJob
   attr_reader :show_id
 
   def perform(show_id)
-    return if total_size > App.album_zip_disk_limit
-
     @show_id = show_id
+
+    return show.update(album_zip_requested_at: nil) if total_size > App.album_zip_disk_limit
+
     create_and_attach_album_zip
     show.update!(album_zip_requested_at: nil)
   end
