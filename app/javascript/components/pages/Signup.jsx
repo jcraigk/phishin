9a37209a21499018650bb9bc448d0aa5e2b131bd
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2025-02-26 20:01:47 -0800

    Disable writes for server maintenance

--- app/javascript/components/pages/Signup.jsx
+++ app/javascript/components/pages/Signup.jsx
@@ -37,71 +37,77 @@ const Signup = () => {
 
   return (
     <PageWrapper>
-      <h1 className="title">Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="label" htmlFor="username">Username</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="email">Email Address</label>
-          <div className="control">
-            <input
-              className="input"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="password">Password</label>
-          <div className="control">
-            <input
-              className="input"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="passwordConfirmation">Confirm Password</label>
-          <div className="control">
-            <input
-              className="input"
-              type="password"
-              id="passwordConfirmation"
-              value={passwordConfirmation}
-              onChange={(e) => setPasswordConfirmation(e.target.value)}
-              required
-            />
-          </div>
-        </div>
-        <div className="field mt-6">
-          <div className="control">
-            <button className="button" type="submit">
-              <FontAwesomeIcon icon={faCheckCircle} className="mr-1" />
-              Sign Up
-            </button>
-          </div>
-        </div>
-      </form>
+      <h2 className="subtitle warning">Email based signups temporarily disabled for server maintenance</h2>
     </PageWrapper>
   );
+
+  // return (
+  //   <PageWrapper>
+  //     <h1 className="title">Sign Up</h1>
+  //     <form onSubmit={handleSubmit}>
+  //       <div className="field">
+  //         <label className="label" htmlFor="username">Username</label>
+  //         <div className="control">
+  //           <input
+  //             className="input"
+  //             type="text"
+  //             id="username"
+  //             value={username}
+  //             onChange={(e) => setUsername(e.target.value)}
+  //             required
+  //           />
+  //         </div>
+  //       </div>
+  //       <div className="field">
+  //         <label className="label" htmlFor="email">Email Address</label>
+  //         <div className="control">
+  //           <input
+  //             className="input"
+  //             type="email"
+  //             id="email"
+  //             value={email}
+  //             onChange={(e) => setEmail(e.target.value)}
+  //             required
+  //           />
+  //         </div>
+  //       </div>
+  //       <div className="field">
+  //         <label className="label" htmlFor="password">Password</label>
+  //         <div className="control">
+  //           <input
+  //             className="input"
+  //             type="password"
+  //             id="password"
+  //             value={password}
+  //             onChange={(e) => setPassword(e.target.value)}
+  //             required
+  //           />
+  //         </div>
+  //       </div>
+  //       <div className="field">
+  //         <label className="label" htmlFor="passwordConfirmation">Confirm Password</label>
+  //         <div className="control">
+  //           <input
+  //             className="input"
+  //             type="password"
+  //             id="passwordConfirmation"
+  //             value={passwordConfirmation}
+  //             onChange={(e) => setPasswordConfirmation(e.target.value)}
+  //             required
+  //           />
+  //         </div>
+  //       </div>
+  //       <div className="field mt-6">
+  //         <div className="control">
+  //           <button className="button" type="submit">
+  //             <FontAwesomeIcon icon={faCheckCircle} className="mr-1" />
+  //             Sign Up
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </form>
+  //   </PageWrapper>
+  // );
 };
 
 export default Signup;
