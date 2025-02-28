6b7c5ae045379f687c5b5759fc75d9afc11cebd3
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2025-02-28 00:26:11 -0800

    hardcode 3000

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,6 @@ require "puma"
 
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
-port ENV.fetch("PORT", 3000)
+port 3000
 environment ENV.fetch("RAILS_ENV", "development")
 plugin :tmp_restart
