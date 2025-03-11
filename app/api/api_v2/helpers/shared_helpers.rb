b53f4580fbc3e3f22fb5ac339117659fc3b715de
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2025-03-10 21:57:49 -0700

    Use ASCII-based sorting on text columns

--- app/api/api_v2/helpers/shared_helpers.rb
+++ app/api/api_v2/helpers/shared_helpers.rb
@@ -1,12 +1,32 @@
 module ApiV2::Helpers::SharedHelpers
   extend Grape::API::Helpers
 
+  # Use case-insensitive ASCII-based sorting on text columns
   def apply_sort(relation, secondary_col = nil, secondary_dir = :asc)
     attribute, direction = params[:sort].split(":")
-    relation = relation.order("#{relation.table_name}.#{attribute} #{direction}")
+    table_name = relation.table_name
+
+    primary_column = relation.connection.schema_cache.columns(table_name).find { |col| col.name == attribute }
+    primary_sort =
+      if primary_column && %i[ string text ].include?(primary_column.type)
+        "LOWER(#{table_name}.#{attribute}) COLLATE \"C\" #{direction}"
+      else
+        "#{table_name}.#{attribute} #{direction}"
+      end
+    relation = relation.order(Arel.sql(primary_sort))
+
     if secondary_col && attribute != secondary_col
-      relation = relation.order("#{relation.table_name}.#{secondary_col} #{secondary_dir}")
+      secondary_column = relation.connection.schema_cache.columns(table_name).find { |col| col.name == secondary_col }
+      secondary_sort =
+        if secondary_column && %i[ string text ].include?(secondary_column.type)
+          "LOWER(#{table_name}.#{secondary_col}) COLLATE \"C\" #{secondary_dir}"
+        else
+          "#{table_name}.#{secondary_col} #{secondary_dir}"
+        end
+
+      relation = relation.order(Arel.sql(secondary_sort))
     end
+
     relation
   end
 
