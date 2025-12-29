bc7687a0cf20ed7dd114a220db873bef7fec77f2
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2025-12-28 17:34:11 -0800

    Cleanup migrations

--- db/migrate/20251225092438_remove_user_fields_from_mcp_tool_calls.rb
+++
@@ -1,7 +0,0 @@
-class RemoveUserFieldsFromMcpToolCalls < ActiveRecord::Migration[8.1]
-  def change
-    remove_reference :mcp_tool_calls, :user, foreign_key: true
-    remove_column :mcp_tool_calls, :user_agent, :string
-    remove_column :mcp_tool_calls, :ip_address, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_25_092438) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_25_070019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
