cb828dde5aace449500a3269c23110e71cb2b2ad
Justin Craig-Kuhn <jcraigk@gmail.com>
2026-04-24 15:08:13 -0700

    CSS fixes

--- app/javascript/components/modals/AppModal.jsx
+++ app/javascript/components/modals/AppModal.jsx
@@ -8,33 +8,13 @@ Modal.setAppElement("#root");
 const AppModal = ({ isOpen, onRequestClose, modalContent }) => {
   if (!modalContent) return null;
 
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      maxHeight: "80vh",
-      maxWidth: "90vw",
-      overflow: "auto",
-      padding: "1.5rem",
-      borderRadius: "6px",
-      zIndex: 106
-    },
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.75)",
-      zIndex: 103
-    }
-  };
-
   return (
     <Modal
       id="app-modal"
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      style={customStyles}
+      className="modal-content"
+      overlayClassName="modal-overlay"
       onClick={(e) => e.stopPropagation()}
     >
       <FontAwesomeIcon
