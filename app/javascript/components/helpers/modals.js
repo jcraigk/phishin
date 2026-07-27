afea062446889e395a967899d38cf9d6059985b1
Justin Craig-Kuhn <jcraigk@gmail.com>
2026-07-26 20:01:01 -0700

    Use monospaced font for taper notes modal

--- app/javascript/components/helpers/modals.js
+++ app/javascript/components/helpers/modals.js
@@ -11,7 +11,7 @@ export const createTaperNotesModalContent = (show) => {
     <div className="wide-modal">
       <h2 className="title">Taper Notes</h2>
       <h3 className="subtitle">{formatDate(show.date)} • {show.venue_name}</h3>
-      <p dangerouslySetInnerHTML={{ __html: (show.taper_notes || "").replace(/\n/g, "<br />") }}></p>
+      <p className="taper-notes" dangerouslySetInnerHTML={{ __html: (show.taper_notes || "").replace(/\n/g, "<br />") }}></p>
     </div>
   );
 };
