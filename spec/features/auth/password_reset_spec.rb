ee687ebb60039a34d0e6ab55f504f9c0e3b35e39
Justin Craig-Kuhn <jcraigk@gmail.com>
2026-09-15 00:20:50 -0700

    Open the reset email in the password reset feature spec instead of reading the token off the user

--- spec/features/auth/password_reset_spec.rb
+++ spec/features/auth/password_reset_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 require "capybara/email/rspec"
 
-describe "Reset Password", :js do
+describe "Reset Password", :enable_sidekiq, :js do
   let(:password) { "Tr3yIsj3dI" }
   let(:new_password) { "Tr3yIsj3dI2" }
   let(:user) { create(:user, password:, password_confirmation: password) }
@@ -15,20 +15,17 @@ describe "Reset Password", :js do
     expect(page).to have_text \
       "Password reset instructions will be sent to the email if it exists"
 
-    # Open the email, click link
-    # (not working due to config issues, so we skip it)
-    # open_email(user.email)
-    # visit current_email.body.match(/https?:\/\/[\S]+/).to_s
+    open_email(user.email)
+    expect(current_email.subject).to eq("Reset Password")
+    reset_url = current_email.body.match(%r{https?://\S+})[0]
+    expect(reset_url).to end_with("/reset-password/#{user.reload.reset_password_token}")
+    visit URI.parse(reset_url).path
 
-    visit "/reset-password/#{user.reload.reset_password_token}"
-
-    # User enters a new password, twice
     fill_in("password", with: new_password)
     fill_in("passwordConfirmation", with: new_password)
     click_on("Reset password")
     expect(page).to have_text("Password reset successfully")
 
-    # User's password is changed
     expect(user.reload.valid_password?(new_password)).to be(true)
   end
 end
